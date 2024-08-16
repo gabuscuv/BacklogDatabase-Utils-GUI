9826111f4830074640d0a0547b664f422e597ea0
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2024-08-16 19:30:59 +0200

    Initial Commit

---
+++ src/Views/GenericForm.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GameListDBUtils_Desktop.Views;
+
+public partial class GenericForm : UserControl
+{
+    public GenericForm()
+    {
+        InitializeComponent();
+    }
+}
